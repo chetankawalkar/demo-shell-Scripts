8f1c20c7499dfa986b5a8033f501c2b560749bce
chetankawalkar <chetanpramodkawalkar@gmail.comr>
2025-07-27 02:05:54 +0530

    added modifid backup file

--- backup.sh
+++ backup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-<<help
- this is a shell script to take backups
- can also be used with cron
-help 
-source_dir= "/home/adminlinux/scripts"
-destination_dir= "/home/adminlinux/backups"
 
-fuction create_backup {
+<< help 
+This is a shell Scriple to take backup
+can also be used with cron
 
-timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
-backup_dir="${destination_dir}/backup_${timestamp}"
+help
+source_dir= "/home/adminlinx/scripts"
+destination_dir= "/home/adminlinx/backups"
 
-zip -r  "${backup_dir}.zip"$source_dir"
-echo "Backup completed"
+function create_backup {
+	timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+
+	backup_dir="${destionation_dir}/backup_${timestamp}"
+
+	zip -r "${backup_dir}.zip" "$source_dir"
+
+	echo "Backup Completed successfully"
+
+}
+
+create_backup $source_dir $distination_dir
